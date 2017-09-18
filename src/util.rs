a1d35c2f674a206d600df9401a07128b8f011757
David Ko <david@barobo.com>
2017-09-18 11:04:14 -0700

    Added missing file

---
+++ src/util.rs
@@ -0,0 +1,33 @@
+use std::mem;
+use std::ops;
+
+/// Convert Vec<Option<T>> into a bitmask of enabled fields.
+///
+/// For instance, 
+/// 
+/// ```
+/// assert!(vec_to_mask(vec![Some(0), None, Some(1)]), 0x05)
+/// ```
+pub fn vec_to_mask<T>(v: &Vec<Option<T>>) -> u8 {
+    let result = v.iter().fold((0, 0), |acc, item| {
+        let (mask, i) = acc;
+        let new_mask = if item.is_some() {
+            mask | (1<<i)
+        } else {
+            mask
+        };
+        (new_mask, i+1)
+    });
+    result.0
+}
+
+/// Convert a mask to a Vec of bools
+pub fn mask_to_vec(mask: u8) -> Vec<bool>
+{
+    let mut bools:Vec<bool> = Vec::new();
+    for i in 0..mem::size_of::<u8>() {
+        bools.push( mask&(1<<i) != 0 );
+    }
+    bools
+}
+
